197aeaa6b356e323431b9f591c746878761c3564
WeryZebra-Yue <20se02ce035@ppsu.ac.in>
2021-09-10 09:26:36 +0530

    Second

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let req = new XMLHttpRequest();
 req.open("GET","https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=71a7a69bc8a74c3181be2e06814ee95a")
 req.send();
 req.onload = () =>{
-    console.log(req)
+    // console.log(req)
     if(req.status == 200){
        let response = JSON.parse(req.response);
-         console.log(response.totalResults/20)
+         console.log(Math.ceil(response.totalResults/20))
          Query('.hello').style.minHeight = "none!important"
        for (const [key, value] of Object.entries(response.articles)) {
         Converting(value)
@@ -13,7 +13,7 @@ req.onload = () =>{
       
     }
     else{
-        console.log(req)
+        // console.log(req)
     }
  
 }
@@ -41,6 +41,7 @@ const Converting = (value) =>{
     let title;
     let urltoImage;
     let content;
+    let href;
     console.log(value)
     for (const [keys, values] of Object.entries(value)) {
             
@@ -53,23 +54,29 @@ const Converting = (value) =>{
         else if(keys == 'description'){
             content = values;
         }
+        else if(keys == 'url'){
+            href = values;
+        }
         
     }
     if(urltoImage){
-        console.log('S')
-        let mainDiv = document.createElement('div');
+       
+        // let mainDiv = document.createElement('div');
         let img = document.createElement('img');
         let desc = document.createElement('desc');
         let titles = document.createElement('span')
         titles.innerHTML = title;
         desc.innerHTML = content;
         img.src = urltoImage;
+        let  a = document.createElement('a')
         titles.classList.add('spand')
         desc.classList.add('desc')
         img.classList.add('postImage')
-        mainDiv.classList.add('post')
-        mainDiv.append(titles,img,desc)
-        Query('.hello').append(mainDiv)
+        a.classList.add('post')
+        a.href = href;
+        
+        a.append(titles,img,desc)
+        Query('.hello').append(a)
     }
 
 }
